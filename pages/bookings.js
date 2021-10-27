2ccaa620983ed5a512b4acafd49572bca5d7f9ec
Glenda Tan <glenda.tan04@rafflesgirlssch.edu.sg>
2021-10-27 11:04:44 +0800

    finishing touches to date

--- pages/bookings.js
+++ pages/bookings.js
@@ -12,7 +12,7 @@ export default function BookUsCalendar() {
     async function createDateRecord({ start, end }) {
         try {
             const user = supabase.auth.user();
-            const updates = { createdat: new Date(), date: start, userid: user.id };
+            const updates = { createdat: new Date(), date: end, userid: user.id };
             
             let { error } = await supabase.from('dates').upsert(updates, { returning: 'minimal'});
             if (error) {

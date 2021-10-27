796a4ce85795515e8fb2011a44b184456e64eb24
Glenda Tan <glenda.tan04@rafflesgirlssch.edu.sg>
2021-10-27 11:02:12 +0800

    added date functionality

--- pages/bookings.js
+++ pages/bookings.js
@@ -3,12 +3,25 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import Link from "next/link";
+import { supabase } from "../utils/supabaseClient";
 
 export default function BookUsCalendar() {
     const localizer = momentLocalizer(moment);
-    const myEventsList = [
-        { start: new Date(), end: new Date(), title: "event0" },
-    ];
+    var myEventsList = [];
+
+    async function createDateRecord({ start, end }) {
+        try {
+            const user = supabase.auth.user();
+            const updates = { createdat: new Date(), date: start, userid: user.id };
+            
+            let { error } = await supabase.from('dates').upsert(updates, { returning: 'minimal'});
+            if (error) {
+                throw error;
+            }
+        } catch (error) {
+            alert(error.message);
+        }
+    }
 
     return (
         <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center m-4">
@@ -19,10 +32,12 @@ export default function BookUsCalendar() {
             <h1>Calendar</h1>
             <Calendar
                 localizer={localizer}
-                events={myEventsList}
                 startAccessor="start"
                 endAccessor="end"
+                selectable={true}
+                events = {myEventsList}
                 style={{ height: "90vh" }}
+                onSelectSlot={createDateRecord}
             />
             <Link href="/">
                 <button className="transition duration-500 ease-in-out w-4/5 md:w-2/5 text-center py-3 focus:outline-none my-1 bg-blue-400 border rounded hover:animate-bounce text-white">

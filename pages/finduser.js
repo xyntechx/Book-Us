71a29c4ef7594d1ef29125efddab245b5ac392c0
xyntechx <xyntechx@gmail.com>
2021-11-22 21:12:22 +0800

    🔨 Refactor code

--- pages/finduser.js
+++ pages/finduser.js
@@ -6,20 +6,21 @@ import { supabase } from "../utils/supabaseClient";
 export default function FindUser() {
     const [results, setResults] = useState([]);
     const [value, setValue] = useState("");
-    
+
     function handleChange(event) {
         event.preventDefault();
         setValue(event.target.value.toLowerCase());
     }
 
     async function checkUsers() {
-        var allUsers = [];
-        const {data, error} = await supabase
-        .from('profiles')
-        .select('username');
+        let allUsers = [];
+        const { data, error } = await supabase
+            .from("profiles")
+            .select("username");
+
         for (const item of data) allUsers.push(item.username); // extract all users from supabase
-        
-        var filteredUsers = [];
+
+        let filteredUsers = [];
         for (const item of allUsers) {
             if (item.toLowerCase().includes(value)) {
                 filteredUsers.push(item);
@@ -27,7 +28,7 @@ export default function FindUser() {
         }
 
         if (filteredUsers.length == 0) {
-            filteredUsers.push('Sorry, no users found.');
+            filteredUsers.push("Sorry, no users found.");
         }
 
         setResults(filteredUsers);
@@ -36,27 +37,37 @@ export default function FindUser() {
     useEffect(checkUsers, [value]);
 
     return (
-        <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center m-4" id="root">
+        <div
+            className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center m-4"
+            id="root"
+        >
             <Head>
                 <title>BookUs</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            
-            <h1 className="text-4xl md:text-6xl font-bold">Who would you like to meet?</h1>
 
-            <form className = "w-4/5 m-20 h-10">
-                <input placeholder = "Enter a username" name = "name" className = "border rounded" onChange={handleChange}></input>
+            <h1 className="text-4xl md:text-6xl font-bold">
+                Who would you like to meet?
+            </h1>
+
+            <form className="w-4/5 m-20 h-10">
+                <input
+                    placeholder="Enter a username"
+                    name="name"
+                    className="border rounded"
+                    onChange={handleChange}
+                ></input>
             </form>
 
-            <p><b>Search Results</b></p>
+            <p>
+                <b>Search Results</b>
+            </p>
             <br></br>
 
             <ul>
-                {
-                    results.map((username, i) => {
-                        return <li>{username}</li>
-                    })
-                }
+                {results.map((username, i) => {
+                    return <li>{username}</li>;
+                })}
             </ul>
 
             <br></br>
